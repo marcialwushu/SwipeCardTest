5169c3b04dcf8beef22f517cc7a287837e7c6f0d
marcialwushu <marcialwushu@gmail.com>
2019-02-05 16:07:14 -0200

    fisrt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,33 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SwipeCardsModule } from 'ng2-swipe-cards';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+// import { HammerConfig } from 'ng2-swipe-cards/src/ng2-swipe-cards.module';
+
+export class HammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    'pan' : { enable: true }
+  }
+}
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    SwipeCardsModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HAMMER_GESTURE_CONFIG, useClass: HammerConfig }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild, TemplateRef, EventEmitter } from '@angular/core';
+import { SwipeCardsModule } from 'ng2-swipe-cards';
 
 @Component({
   selector: 'app-home',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  // styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('cardLog') cardLogContainer: any;
+  @ViewChild('tinderCardLog') tinderCardLogContainer: any;
 
 }
