c965a1a441acb8820deb99f116ac122602f785f0
marcialwushu <marcialwushu@gmail.com>
2019-02-05 18:20:58 -0200

    added functions in this methods - prperty map does exist on type Observable<Response>

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, ViewEncapsulation, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { SwipeCardsModule } from 'ng2-swipe-cards';
 import { Http } from '@angular/http';
-import 'rxjs/Rx';
+// import 'rxjs/Rx';
 import { element } from 'protractor';
 import { 
   StackConfig,
@@ -12,6 +12,7 @@ import {
   SwingStackComponent,
   SwingCardComponent } from 'angular2-swing';
 
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-home',
@@ -42,23 +43,68 @@ export class HomePage {
   }
 
   ngAfterViewInit(){
+    this.swingStack.throwin.subscribe((event: DragEvent) => {
+      event.target.style.background = '#ffffff';
+    });
 
+    this.cards = [{ email: ''}];
+    this.addNewCards(1);
   }
 
+  
+  /**
+   * Called whenever we drag an element
+   * @param element 
+   * @param x 
+   * @param y 
+   * @param r 
+   */
   onItemNove(element, x, y, r) {
+    var color = '';
+    var abs = Math.abs(x);
+    let min = Math.trunc(Math.min(16*16 - abs, 16*16));
+    let hexCode = this.decimalToHex(min, 2);
 
+    if (x < 0) {
+      color = '#FF' + hexCode + hexCode;
+    } else {
+      color = '#' + hexCode + 'FF' + hexCode;
+    }
+
+    element.style.background = color;
+    element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
   }
 
+  /**
+   * Connected through HTML
+   * @param like 
+   */
   voteUp(like: boolean) {
-
+    let removeCard = this.cards.pop();
+    this.addNewCards(1);
+    if (like) {
+      this.recentCard = 'You liked: ' + removeCard.email;
+    } else {
+      this.recentCard = 'You disliked: ' + removeCard.email;
+    }
   }
 
+  /**
+   * Add new cards to our array
+   * @param count 
+   */
   addNewCards(count: number) {
-
+    this.http.get('https://randomuser.me/api/?results=' + count)
+    .map(data => data.json().results)
+    .subscribe(result => {
+      for (let val of result) {
+        this.cards.push(val);
+      }
+    })
   }
 
   decimalToHex(d, padding) {
-    
+
   }
 
 
